51e13e731ce896cc8008c0d390f4e3f0817c708f
webDog <2725550609@qq.com>
2022-02-20 17:59:43 +0800

    列表样式调整

--- theblog/src/components/list/views/List.vue
+++ theblog/src/components/list/views/List.vue
@@ -3,17 +3,36 @@
     <el-row :gutter="12">
       <el-col :span="24">
         <ul class="list_ul">
-          <li class="list_li" v-for="(item, index) in get_articleList" :key="index" @click="to_article(item._id)">
+          <li
+            class="list_li"
+            v-for="(item, index) in get_articleList"
+            :key="index"
+            @click="to_article(item._id)"
+          >
             <el-card class="list_li_div" shadow="hover">
               <el-image
                 class="el_card_el_image"
                 :src="item.coverRequirePath"
                 fit="contain"
               ></el-image>
-              <div class="el_card_title">{{item.title}}</div>
-              <div class="el_card_generalization">{{item.synopsis}}</div>
-              <el-tag class="el_card_time" type="info">{{item.date}}</el-tag>
-              <el-tag class="el_card_tag">{{item.category}}</el-tag>
+              <ul class="textIntroduction_ul">
+                <li class="textIntroduction_li">
+                  <div class="el_card_title">{{ item.title }}</div>
+                </li>
+                <li class="textIntroduction_li">
+                  <p class="el_card_generalization">{{ item.synopsis }}</p>
+                </li>
+                <li class="textIntroduction_li">
+                  <div class="textIntroduction_li_div">
+                    <el-tag class="el_card_time" type="info">
+                      {{ item.date }}
+                    </el-tag>
+                  </div>
+                  <div class="textIntroduction_li_div">
+                    <el-tag class="el_card_tag">{{ item.category }}</el-tag>
+                  </div>
+                </li>
+              </ul>
             </el-card>
           </li>
         </ul>
